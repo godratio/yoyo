fa122104498c3c48688c82a8eb189a6a9334857d
Ray Garner <raygarner13@gmail.com>
2018-12-10 19:31:25 +0900

    easier integration setup

--- yoyoyo.h
+++ yoyoyo.h
@@ -3,6 +3,9 @@
 //TODO(Ray):Move this dependence from here to yoyomemory
 #include "../api/api_include.h"
 
+#include "meow_hash/meow_intrinsics.h"
+#include "meow_hash/meow_hash.h"
+
 #define Assert(Expression) if(!(Expression)){*(int *)0 = 0;};
 #define ArrayCount(Array) sizeof(Array) / sizeof((Array)[0])
 
@@ -14,4 +17,4 @@
 #include "yoyoyo_ascii_vector.h"
 #include "yoyoyo_math.h"
 #include "yoyoyo_hashtable.h"
-
+#include "yoyoyo_scene.h"
